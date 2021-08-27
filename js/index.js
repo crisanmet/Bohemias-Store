aa443013ea0f707f77aa65e7d70f52a356da7951
Cristian Sancricca <cristiansancricca@gmail.com>
2021-08-27 00:19:36 -0300

    Se agrega ScrollReveal

---
+++ js/index.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+sr.reveal(".card", {
+  duration: 3000,
+  origin: "left",
+  distance: "400px",
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".iconos__container", {
+  duration: 3000,
+  origin: "right",
+  distance: "500px",
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".scroll__hero", {
+  duration: 3000,
+  origin: "right",
+  distance: "600px",
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".remeras__card", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".scroll__boton", {
+  duration: 2000,
+  origin: "right",
+  delay: 500,
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".scroll__faqs", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "4000px",
+});
